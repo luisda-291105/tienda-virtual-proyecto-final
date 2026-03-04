621bd562e7873f0003f6fbcfcae8c968cd841e64
luisDaniel <estudiosluisperez@gmail.com>
2026-03-03 22:09:14 -0500

    fix se le elimino una capa que estaba en el backend

--- BACKEND_TIENDA_NODE_MYSQL/BACKEND_TIENDA_NODE_MYSQL/src/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express")
-const { login } = require("../controllers/authController")
-
-const router = express.Router()
-
-// POST /api/login
-router.post("/login", login)
-
-module.exports = router
--- BACKEND_TIENDA_NODE_MYSQL/BACKEND_TIENDA_NODE_MYSQL/src/utils/errorHandler.js
+++
@@ -1,6 +0,0 @@
-const handleError = (res, error, message = "Error interno del servidor") => {
-  console.error("Error:", error)
-  res.status(500).json({ message: message + ": " + error.message })
-}
-
-module.exports = { handleError }
